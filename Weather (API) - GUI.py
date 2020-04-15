0446e7c20935413ab4b87d5e49bce6c69acd44c6
Xala <56874379+SaharItzhak@users.noreply.github.com>
2020-04-15 10:36:01 +0300

    Update Weather (API) - GUI.py

--- Weather (API) - GUI.py
+++ Weather (API) - GUI.py
@@ -40,9 +40,10 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-background_image = tk.PhotoImage(file='landscape.png')
-background_label = tk.Label(root, image=background_image)
-background_label.place(relwidth=1, relheight=1)
+# BACKGROUND IMAGE 
+# background_image = tk.PhotoImage(file='landscape.png')
+# background_label = tk.Label(root, image=background_image)
+# background_label.place(relwidth=1, relheight=1)
 
 frame = tk.Frame(root, bg='#80c1ff', bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
